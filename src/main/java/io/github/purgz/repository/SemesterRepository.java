b07e08a4c630fc975c0b7c9c989d308feb81b0eb
Henry Brooks <henbrooks6@gmail.com>
2024-05-03 12:19:01 +0100

    get semesters by year endpoint

--- src/main/java/io/github/purgz/repository/SemesterRepository.java
+++ src/main/java/io/github/purgz/repository/SemesterRepository.java
@@ -1,6 +1,8 @@
 package io.github.purgz.repository;
 
+import io.github.purgz.domain.LeagueYear;
 import io.github.purgz.domain.Semester;
+import java.util.List;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,6 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface SemesterRepository extends JpaRepository<Semester, Long> {}
+public interface SemesterRepository extends JpaRepository<Semester, Long> {
+    List<Semester> findAllByYear(LeagueYear year);
+}
