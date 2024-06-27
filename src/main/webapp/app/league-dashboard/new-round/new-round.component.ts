d599105cdfca97c691bbea3d0f834b4e3131533e
Henry Brooks <henbrooks6@gmail.com>
2024-06-27 11:52:23 +0100

    start on weekly player modal

--- src/main/webapp/app/league-dashboard/new-round/new-round.component.ts
+++ src/main/webapp/app/league-dashboard/new-round/new-round.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 import { LeagueYearService } from '../../entities/league-year/service/league-year.service';
 import { ILeagueYear } from '../../entities/league-year/league-year.model';
@@ -11,6 +11,8 @@ import { LeagueDataModel } from '../service/league-data.model';
 //service
 import { LeagueDataService } from '../service/league-data.service';
 import { SemesterScoreService } from '../../entities/semester-score/service/semester-score.service';
+
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'new-round',
   templateUrl: './new-round.component.html',
@@ -19,11 +21,32 @@ export class NewRoundComponent implements OnInit {
   constructor(
     private leagueYearService: LeagueYearService,
     public leagueDataService: LeagueDataService,
-    public semesterScoreService: SemesterScoreService
+    public semesterScoreService: SemesterScoreService,
+    private modalService: NgbModal
   ) {}
 
   //work on modal for new match results
   //updating the league model correctly.
 
+  @ViewChild('testModal') playerModal?: ElementRef;
+
+  playerModalRef?: NgbModalRef;
+
   ngOnInit(): void {}
+
+  openModal(): void {
+    this.playerModalRef = this.modalService.open(this.playerModal);
+  }
+
+  modalSave(): void {
+    //save changes to players and close
+
+    this.playerModalRef?.close();
+  }
+
+  closeModal(): void {
+    //close without saving
+
+    this.playerModalRef?.close();
+  }
 }
